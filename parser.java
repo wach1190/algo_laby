7aac43d442a934700b157749b58917c414fd29b6
Alexandre Wachholder <wach1190@hotmail.com>
2014-12-01 16:10:28 +0100

    First shot on parser

--- parser.java
+++ parser.java
@@ -1,1 +1,13 @@
-//...
+/* 
+ * (C) Copyrights ${2014} Wachholder Alexandre and Jaspe Juena.
+ * Algorithmic Project 1 : Labyrinthus
+ */
+
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Random; // maybe useless here, to see ...
+
+
+// First part : decode text labyrinthus file
+
+// http://www.infres.enst.fr/~hudry/coursJava/fichiersEtSaisies/lireFichierTexte.html
