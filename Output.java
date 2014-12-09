dfcef3ed0c6701ba91150506534448411ababbdd
Alexandre Wachholder <wach1190@hotmail.com>
2014-12-09 17:25:00 +0100

    Résultat temporaire de l'output, à vérifier

--- Output.java
+++ Output.java
@@ -5,12 +5,18 @@ import java.lang.*;
 import java.io.File;
 import parser.java;
 
-public class Main{ // on va devoir mettre en argument la classe labyrinthe, afin de get toutes les infos nécessaires à l'output
+public class OutputF{ // on va devoir mettre en argument la classe labyrinthe, afin de get toutes les infos nécessaires à l'output
     // espace attributs ==> to see later
     File f = new File("Output_LABY"); // output file
-    Parser object = new Parser
+    Labyrinthe object;
+    int i = 1; // compteur d'emplacements, incrémenté à chaque impression
     
     List<Emplacement> historic; // historique des emplacements à renvoyer
+    
+    // Constructeur avec argument :
+    public static void main(Labyrinthe toOut){
+        object = toOut; // le laby passé en argument est celui qu'on analysera pour l'output
+    }
     public makeOutput(){
         System.out.print("Le labyrinthe à une dimension de : " + "8x8"); // get la dimension
         System.out.print(" Il contient" + "3"+"monstres et " + "4"+ "bonbons"); // get monstres et bonbons
@@ -19,7 +25,10 @@ public class Main{ // on va devoir mettre en argument la classe labyrinthe, afin
         System.out.print("Les bonbons en positions ...");
         System.out.print("Déplacements de M.Pakkuman :");
         // Suivi de la liste des déplacements, à get dans historic ...
-        
+        // Donc normalement, je choppe la liste d'emplacements contenu dans la classe labyrinthe, et je la print, tout simplement
+        for emplacement in object.emplacements {
+            System.out.print(i +". "+ "(" + emplacement.getX + ", " +emplacement.getY + ")" + "get direction");
+        }
 //        historic = Parser.getListEmplacements(); // on get cette fameuse liste
 //        // Maintenant on doit print une ligne par emplacement.
 //        for(int i = 0; i < 10; ++i){
